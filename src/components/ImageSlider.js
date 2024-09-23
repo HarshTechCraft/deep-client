5999ea05f6f99b2a385c9d7a7a8ee79b39bb697f
HarshTechCraft <harshdvadhavana26@gmail.com>
2024-09-23 23:31:58 +0530

    Harsh

--- src/components/ImageSlider.js
+++ src/components/ImageSlider.js
@@ -30,7 +30,7 @@ const ImageSlider = () => {
       slidesToShow: 1,
       slidesToScroll: 1,
       autoplay: true,
-      autoplaySpeed: 2000,
+      autoplaySpeed: 2500,
       beforeChange: (current, next) => setCurrentSlide(next),
     };
   
