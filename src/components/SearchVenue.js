5a362a5e3b069bcb7e99e73ccb958bd3fc4491a6
HarshTechCraft <harshdvadhavana26@gmail.com>
2024-09-25 15:24:58 +0530

    Harsh

--- src/components/SearchVenue.js
+++ src/components/SearchVenue.js
@@ -1,12 +1,10 @@
 import React, { useEffect, useState } from 'react';
-import { Dropdown, DropdownButton, Form, FormControl } from 'react-bootstrap';
-import '../style/SearchForm.css'; // Import your custom CSS
+import '../style/SearchForm.css';
 
 const SearchVenue = () => {
   const [inputValue, setInputValue] = useState('');
   const [suggestions, setSuggestions] = useState([]);
   const [showSuggestions, setShowSuggestions] = useState(false);
-  const [selectedSuggestion, setSelectedSuggestion] = useState('');
 
   const predefinedKeywords = [
     'keyword1',
@@ -23,33 +21,21 @@ const SearchVenue = () => {
       setShowSuggestions(true);
     } else {
       setShowSuggestions(false);
-      setSelectedSuggestion(''); // Clear selection when input is empty
     }
   }, [inputValue]);
 
-  const handleSelect = (suggestion) => {
-    setInputValue(suggestion); // Update input value on selection
-    setSelectedSuggestion(suggestion);
-    setShowSuggestions(false); // Hide suggestions after selection
-  };
-
   return (
     <div className="search-container">
-      <Form className="search-form">
+      <form className="search-form">
         <div className="input-group">
           <label htmlFor="activity">What are you planning?</label>
-          <DropdownButton
-            id="dropdown-basic-button"
-            title={selectedSuggestion || "Select Activity"} // Display selected activity or placeholder
-            onSelect={handleSelect}
-          >
-            {suggestions.map((suggestion, index) => (
-              <Dropdown.Item key={index} eventKey={suggestion}>
-                {suggestion}
-              </Dropdown.Item>
-            ))}
-          </DropdownButton>
-          {/* Consider adding a custom "Add New" option if applicable */}
+          <input
+            type="text"
+            id="activity"
+            value={inputValue}
+            onChange={(e) => setInputValue(e.target.value)}
+            placeholder="Enter your activity"
+          />
         </div>
         <div className="input-group">
           <label htmlFor="location">Where?</label>
@@ -64,20 +50,12 @@ const SearchVenue = () => {
             Search ➔
           </button>
         </div>
-      </Form>
+      </form>
 
-      {/* Display filtered suggestions below the dropdown if needed */}
-      {showSuggestions && (
-        <ul className="suggestion-list">
-          {suggestions.map((suggestion, index) => (
-            <li key={index} onClick={() => handleSelect(suggestion)}>
-              {suggestion}
-            </li>
-          ))}
-        </ul>
-      )}
+      
     </div>
   );
 };
+
 
 export default SearchVenue;
