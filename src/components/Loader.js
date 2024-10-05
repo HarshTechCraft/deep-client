baa0df7b0478567f146a76955d98774d106f50b0
HarshTechCraft <harshdvadhavana26@gmail.com>
2024-10-05 12:09:08 +0530

    h

--- src/components/Loader.js
+++ src/components/Loader.js
@@ -1,32 +1,7 @@
-// import { WifiLoader } from "react-awesome-loaders";
 import { Triangle } from "react-loader-spinner";
 import '../style/Loader.css'
 export const Loader = () => {
   return (
-    // <div
-    //   style={{
-    //     position: "fixed",
-    //     top: 0,
-    //     left: 0,
-    //     width: "100vw",
-    //     height: "100vh",
-    //     display: "flex",
-    //     justifyContent: "center",
-    //     alignItems: "center",
-    //     backgroundColor: "rgba(255, 255, 255, 0.2)",
-    //     backdropFilter: "blur(2px)",
-    //     zIndex: 9999,
-    //   }}
-    // >
-    //   <WifiLoader
-    //     background={"transparent"}
-    //     desktopSize={"70em"}
-    //     mobileSize={"70em"}
-    //     text={""}
-    //     backColor="black"
-    //     frontColor="#FFB933"
-    //   />
-    // </div>
     <div
       style={{
         position: "fixed",
@@ -49,7 +24,7 @@ export const Loader = () => {
         color="#FFB933"
         ariaLabel="triangle-loading"
         wrapperStyle={{}}
-        wrapperClass=""
+        wrapperClass="okmdcik"
       />
     </div>
   );
