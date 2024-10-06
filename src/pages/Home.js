e55b00c13beadac75c700da6011e7765c2c2a846
HarshTechCraft <harshdvadhavana26@gmail.com>
2024-10-06 22:27:59 +0530

    h

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,44 +1,44 @@
-import React, { useEffect, useState } from 'react';
-import Header from '../components/Header';
-import ImageSlider from '../components/ImageSlider';
-import SearchVenue from '../components/SearchVenue';
-import Footer from '../components/Footer';
-import Section1 from '../components/Section1';
-import '../style/Home.css'
+import React, { useEffect, useState } from "react";
+import Header from "../components/Header";
+import ImageSlider from "../components/ImageSlider";
+import SearchVenue from "../components/SearchVenue";
+import Footer from "../components/Footer";
+import Section1 from "../components/Section1";
+import "../style/Home.css";
+import { Zoom } from "react-toastify";
 
 function Home() {
   const [scrollOpacity, setScrollOpacity] = useState(0);
 
   useEffect(() => {
     const handleScroll = () => {
-      const scrollY = window.scrollY+10;
-      const maxScroll = 700; 
+      const scrollY = window.scrollY + 10;
+      const maxScroll = 700;
 
       // Calculate opacity based on scroll position (between 0 and 1)
       const newOpacity = Math.min(scrollY / maxScroll, 1); // Caps at 1 when scrollY reaches maxScroll
       setScrollOpacity(newOpacity);
     };
 
-    window.addEventListener('scroll', handleScroll);
+    window.addEventListener("scroll", handleScroll);
 
     return () => {
-      window.removeEventListener('scroll', handleScroll);
+      window.removeEventListener("scroll", handleScroll);
     };
   }, []);
 
   return (
     <div>
-      {/* Apply the fixed class and dynamically set background color */}
       <div className="fixed-section">
-        {/* Adding the darkening effect as a background overlay */}
         <div
           className="background-overlay"
-          style={{ backgroundColor: `rgba(0, 0, 0, ${scrollOpacity})` }}
+          style={{ backgroundColor: `rgba(0, 0, 0, ${scrollOpacity})` , zIndex: scrollOpacity > 0.57 ? "99" : "0" }}
         ></div>
 
         <ImageSlider />
-        <SearchVenue />
+
       </div>
+      <SearchVenue />
 
       <Header />
 
